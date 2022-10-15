c0f18811ac7be37e5072e5b41439d25aa0e545d2
PedroKravetz <p.v.kravetz@gmail.com>
2022-10-15 14:03:31 -0300

    Projeto Palindromos adicionado

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String palavra = sc.nextLine();
+
+        if (palavra.equals(reverse(palavra))) {
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
+    }
+
+    /**
+     * Faz a String ser invertida, ou seja, de trás para frente
+     *
+     * @param string String - A String que será invertida
+     * @return String - A String invertida
+     */
+    public static String reverse(String string) {
+        if (string.length() == 0)
+            return "";
+        return reverse(string.substring(1)) + string.charAt(0);
+    }
+}
